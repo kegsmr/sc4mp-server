ba14c40c674ce3504ec12d2e9ff368f0263ddd67
keggre <kegocandy@gmail.com>
2023-01-08 08:09:12 +0100

    config stuff

--- dmrserver.py
+++ dmrserver.py
@@ -12,14 +12,19 @@ import string
 # Path to the resources subdirectory
 dmr_resources_path = "resources"
 
-
 # Default config values
 default_host = socket.gethostname()
 default_port = 7246
+default_server_id =''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for i in range(16))
+default_server_name = os.getlogin() + " on " + socket.gethostname()
+default_server_description = "Join and build your city.\n\nRules:\n- Feed the llamas\n- Balance your budget\n- Do uncle Vinny some favors"
 
 # Config constants
 DMR_HOST = None
 DMR_PORT = None
+DMR_SERVER_ID = None
+DMR_SERVER_NAME = None
+DMR_SERVER_DESCRIPTION = None
 
 # Hard-coded constants
 DMR_SEPARATOR = b"<SEPARATOR>"
@@ -96,24 +101,37 @@ def load_config():
 
 	print("[DMR] Loading config...")
 
-	configpath = os.path.join("_DMR", "serverconfig.ini")
+	config_path = os.path.join("_DMR", "serverconfig.ini")
+
 	try:
+
 		config = configparser.RawConfigParser()
-		config.read(configpath)
+		config.read(config_path)
 
 		DMR_HOST = config.get('server', "host")
 		DMR_PORT = int(config.get('server', 'port'))
+		DMR_SERVER_ID = config.get('server', "server_id")
+		DMR_SERVER_NAME = config.get('server', "server_name")
+		DMR_SERVER_DESCRIPTION = config.get('server', "server_description")
+
 	except:
+
 		config.remove_section('server')
 		config.add_section('server')
 		config.set('server', 'host', default_host)
 		config.set('server', 'port', default_port)
+		config.set('server', 'server_id', default_server_id)
+		config.set('server', 'server_name', default_server_name)
+		config.set('server', 'server_description', default_server_description)
 		
-		with open(configpath, 'wt') as configfile:
-			config.write(configfile)
+		with open(config_path, 'wt') as config_file:
+			config.write(config_file)
 
 		DMR_HOST = default_host
 		DMR_PORT = default_port
+		DMR_SERVER_ID = default_server_id
+		DMR_SERVER_NAME = default_server_name
+		DMR_SERVER_DESCRIPTION = default_server_description
 
 
 """TODO
@@ -192,6 +210,12 @@ def start_server():
 
 		if (request== "ping"):
 			ping(c)
+		elif (request == "server_id"):
+			send_server_id(c)
+		elif (request == "server_name"):
+			send_server_name(c)
+		elif (request == "server_description"):
+			send_server_description(c)
 		elif (request == "plugins"):
 			send_plugins(c)
 		elif (request == "regions"):
@@ -211,6 +235,24 @@ def start_server():
 """TODO"""
 def ping(c):
 	c.send(b"pong")
+	c.close
+
+
+def send_server_id(c):
+	"""TODO"""
+	c.send(DMR_SERVER_ID.encode())
+	c.close
+
+
+def send_server_name(c):
+	"""TODO"""
+	c.send(DMR_SERVER_NAME.encode())
+	c.close
+
+
+def send_server_description(c):
+	"""TODO"""
+	c.send(DMR_SERVER_DESCRIPTION.encode())
 	c.close
 
 
@@ -244,15 +286,6 @@ def send_regions(c):
 	filename = os.path.join("_DMR", os.path.join("DMRTemp", "Regions.zip"))
 
 	send_or_cached(c, filename)
-	
-
-def send_or_cached(c, filename):
-	"""TODO"""
-	c.send(md5(filename).encode())
-	if (c.recv(DMR_BUFFER_SIZE).decode() == "not cached"):
-		send_file(c, filename)
-	else:
-		c.close()
 
 
 def delete(c):
@@ -297,6 +330,15 @@ def save(c):
 	receive_file(c, filename)
 
 	c.close()
+
+
+def send_or_cached(c, filename):
+	"""TODO"""
+	c.send(md5(filename).encode())
+	if (c.recv(DMR_BUFFER_SIZE).decode() == "not cached"):
+		send_file(c, filename)
+	else:
+		c.close()
 
 
 """TODO
