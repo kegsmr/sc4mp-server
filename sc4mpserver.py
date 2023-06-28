cdb94da4a066220990da44bc7056815ccb07cd1e
keggre <kegocandy@gmai.com>
2023-06-27 21:23:55 -0400

    changed official server

--- sc4mpserver.py
+++ sc4mpserver.py
@@ -18,7 +18,7 @@ import inspect
 
 SC4MP_VERSION = (0,1,0)
 
-SC4MP_SERVERS = [("64.223.232.94", 7246)]
+SC4MP_OFFICIAL_SERVERS = [("64.223.232.94", 7240)]
 
 SC4MP_CONFIG_PATH = None
 SC4MP_LOG_PATH = "sc4mpserver-" + datetime.now().strftime("%Y%m%d%H%M%S") + ".log"
