509fe9a5a5a8d76930df1a41afffa3c22d121f1d
keggre <kegocandy@gmai.com>
2024-06-30 23:53:01 -0400

    Merge branch 'feature'

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-import glob
 import os
 import platform
 import shutil
 import struct
-import sys
 from datetime import datetime
-from distutils.core import setup
 
-import py2exe
+import PyInstaller.__main__ as pyinstaller
+from pyinstaller_versionfile import create_versionfile
 
 import sc4mpserver
 
 
+TITLE = "SC4MP Server"
+NAME = "sc4mpserver.exe"
 VERSION = sc4mpserver.SC4MP_VERSION
+PUBLISHER = "SimCity 4 Multiplayer Project"
+DESCRIPTION = "Multiplayer gameserver for SimCity 4"
+LICENSE = "MIT-0"
 DIST = "dist" + str(8 * struct.calcsize("P"))
 
 
 def main():
 
-	# MAKE DISTRIBUTION DIRECTORY IF IT DOES NOT YET EXIST
+	# Make distribution directory if it does not yet exist
+	print(f"Preparing distribution directory at \"{DIST}\"")
 	if not os.path.exists(DIST):
 		os.makedirs(DIST)
 
-	# PURGE THE DISTRIBUTION DIRECTORY
+	# Purge the distribution directory
 	for item in os.listdir(DIST):
 		item = os.path.join(DIST, item)
 		if (os.path.isfile(item)):
@@ -69,41 +73,16 @@ def main():
 	shutil.copytree(os.path.join("dist", "sc4mpserver"), DIST, dirs_exist_ok=True)
 
 
-	# COPY LICENSE AND README TO DISTRIBUTION DIRECTORY
+	# Copy extra files to distribution directory
 	print(f'Copying extra files to "{DIST}"...')
 	shutil.copytree("resources", os.path.join(DIST, "resources"), dirs_exist_ok=True)
 	shutil.copy("License.txt", DIST)
 	shutil.copy("Readme.html", DIST)
 
 	# CREATE A ZIP ARCHIVE OF THE DISTRIBUTION IF REQUESTED
-	input("Press <Enter> to create a zip archive of the distribution...")
 	destination = os.path.join(os.path.join("builds", "sc4mp-server-" + platform.system().lower() + "-" + str(8 * struct.calcsize("P")) + "-v" + VERSION + "." + datetime.now().strftime("%Y%m%d%H%M%S")))
 	print('Creating zip archive of "' + DIST + '" at "' + destination + '"')
 	shutil.make_archive(destination, "zip", DIST)
-
-
-def find_data_files(source,target,patterns):
-    """Locates the specified data-files and returns the matches
-    in a data_files compatible format.
-
-    source is the root of the source data tree.
-        Use '' or '.' for current directory.
-    target is the root of the target data tree.
-        Use '' or '.' for the distribution directory.
-    patterns is a sequence of glob-patterns for the
-        files you want to copy.
-    """
-    if glob.has_magic(source) or glob.has_magic(target):
-        raise ValueError("Magic not allowed in src, target")
-    ret = {}
-    for pattern in patterns:
-        pattern = os.path.join(source,pattern)
-        for filename in glob.glob(pattern):
-            if os.path.isfile(filename):
-                targetpath = os.path.join(target,os.path.relpath(filename,source))
-                path = os.path.dirname(targetpath)
-                ret.setdefault(path,[]).append(filename)
-    return sorted(ret.items())
 
 
 if __name__ == "__main__":
